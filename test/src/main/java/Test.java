d95ce4286af847db48ebf6af6406ce9864ed3105
yangyh <451561292@qq.com>
2020-06-03 09:58:02 +0800

    提交

--- test/src/main/java/Test.java
+++ test/src/main/java/Test.java
@@ -8,34 +8,7 @@ public class Test {
 
     public static void main(String args[]) throws NoSuchFieldException, IllegalAccessException {
 
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1,
-                3,
-                0,
-                TimeUnit.SECONDS, new LinkedBlockingQueue<>()
-        );
 
-
-        for (int i = 1; i <= 4; i++) {
-            int finalI = i;
-            Runnable runnable = () -> {
-                System.out.println("线程" + finalI + "进来了");
-                try {
-                    // 睡眠3秒，模拟线程占用
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("线程" + finalI + "执行完成");
-            };
-            threadPoolExecutor.submit(runnable);
-        }
-        threadPoolExecutor.shutdown();
-        // 线程1进来了
-        // 线程3进来了
-        // 线程3执行完成
-        // 线程1执行完成
-        // 线程2进来了
-        // 线程2执行完成
 
     }
 
